bba46acfcf654e6ca78ef4c663b52c4ee8b6cbf7
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-08-24 21:43:20 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,6 +1,37 @@
 def main():
     # Escribe el código adecuado para completar el programa
-    pass
+x = int(input("Input the first number: "))
+y = int(input("Input the second number: "))
+z = int(input("Input the third number: "))
+
+if (x>y>z):
+    print(z)
+    print(y)
+    print(x)
+
+if (x>z>y):
+    print(y)
+    print(z)
+    print(x)
+
+if (y>x>z):
+    print(z)
+    print(x)
+    print(y)
+
+if (y>z>x):
+    print(x)
+    print(z)
+    print(y)
+
+if (z>y>x):
+    print(x)
+    print(y)
+    print(z)
+
+if (z>x>y):
+    print(y)
+    print(x)
 
 
 if __name__=='__main__':
